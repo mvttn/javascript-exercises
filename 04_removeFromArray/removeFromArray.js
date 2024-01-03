d15329d69da4e4eddb055b3fd032e160eb2afd00
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-03 21:13:18 +1100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...ArgsToRemove) {
+  const newArray = [];
+  for (const element of array) {
+    if (!ArgsToRemove.includes(element)) {
+      newArray.push(element);
+    }
+  }
+  return newArray;
 };
 
 // Do not edit below this line
