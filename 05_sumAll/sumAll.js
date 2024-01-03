0a267d489675288466d9adeff10e85b97a82483c
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-03 22:55:35 +1100

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (firstNum, secondNum) {
+  if (typeof firstNum !== "number" || typeof secondNum !== "number") {
+    return "ERROR";
+  }
+  if (firstNum < 0 || secondNum < 0) {
+    return "ERROR";
+  }
 
+  sum = 0;
+  if (firstNum < secondNum) {
+    for (let numToAdd = firstNum; numToAdd <= secondNum; numToAdd++) {
+      sum += numToAdd;
+    }
+  } else {
+    for (let numToAdd = secondNum; numToAdd <= firstNum; numToAdd++) {
+      sum += numToAdd;
+    }
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
