b01169eda289ee0cc6599f3c7c55542932703972
mvttn <85101143+mvttn@users.noreply.github.com>
2024-01-04 20:44:24 +1100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.toLowerCase().replace(/[^0-9a-z]/g, "");
+  // Define pointers
+  i = 0;
+  j = string.length - 1;
 
+  while (i < j) {
+    if (string[i] !== string[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
